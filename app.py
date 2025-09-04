28618333b08bfe3e2626f5afaf6c787d34fbe95c
Nikhil-23082004 <nandanavanamnikhil@gmail.com>
2025-09-04 16:17:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -181,6 +181,7 @@ def process():
         return jsonify({"error": f"Processing error: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', debug=True)
+
 
 
